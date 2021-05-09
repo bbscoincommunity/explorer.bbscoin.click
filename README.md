28fcb98c8b984098c895d9f9871df25b3a942b25
Jou M <djem@hotmail.com>
2021-05-09 14:35:11 -0400

    Front end assets

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# explorer.bbscoin.click
-BBSCoin blockchain explorer hosted front-end from repository: bbscoin-blockchain-explorer on explorer.bbscoin.click
+# BBSCoin Blockchain Explorer
+Block explorer for BBSCoin CryptoNote based cryptocurrency.
+
+#### Installation
+
+1) It takes data from daemon bbscoind. It should be accessible from the Internet. Run bbscoind with open port as follows:
+```bash
+./bbscoind --enable-cors=* --enable_blockexplorer
+```
+2) Just upload to your website and change 'api' variable in config.js to point to your daemon.
---
+++ config.js
@@ -0,0 +1,18 @@
+//var api = 'https://remotenode.bbscoin.click';
+var api = 'https://explorer.bbscoin.click/api';
+var blockTargetInterval = 120;
+var coinUnits = 100000000;
+var symbol = 'BBS';
+var refreshDelay = 30000;
+var networkStat = {
+    'bbs': [
+		['pool.bbscoin.click', 'https://explorer.bbscoin.click'],
+		['pool.bbscoin.xyz', 'https://explorer.bbscoin.xyz'],
+		['bbscoinpool.club', 'https://explorer.bbscoin.xyz/proxy/us.bbscoinpool.club:8117'],
+		['bbs.dreampool.info', 'https://bbs.dreampool.info/api'],
+		['cryptoknight.cc/bbs', 'https://explorer.bbscoin.xyz/proxy/78.46.85.142:2778'],
+		['bbs.gonspool.com', 'https://explorer.bbscoin.xyz/proxy/bbs.gonspool.com:8117'],
+		['pool-marine.ga/bbs', 'https://explorer.bbscoin.xyz/proxy/pool-marine.ga:8028'],
+		['bbs.howpool.com', 'https://explorer.bbscoin.xyz/proxy/bbs.howpool.com:8117']
+    ]
+};
